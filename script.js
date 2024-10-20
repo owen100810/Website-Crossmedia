61003b665868f94c4c93a0bb0ce48bb0613573b3
owen100810 <140530192+owen100810@users.noreply.github.com>
2024-10-20 12:14:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
- document.getElementById("rate-button").addEventListener("click", function() {
-    window.location.href = "https://docs.google.com/forms/d/1YehElZc0LLJgXIiGXA5gbbOdFdPYBaI1J171-aj_R4k/prefill";
-  });
+
