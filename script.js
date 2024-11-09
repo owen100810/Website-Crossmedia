0b9db95b88b16c4ac39340e1ec0b9e96780b79f3
owen100810 <140530192+owen100810@users.noreply.github.com>
2024-11-09 17:34:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
 
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
