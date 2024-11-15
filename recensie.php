67776b5476a2c7475934ccb93c72996ed4c8b124
owen100810 <140530192+owen100810@users.noreply.github.com>
2024-11-15 13:32:19 +0100

    Update recensie.php

--- recensie.php
+++ recensie.php
@@ -1,17 +1,16 @@
 <?php
-// Ontvang de data uit het formulier
 $naam = $_POST['naam'];
 $email = $_POST['email'];
 $recensie = $_POST['recensie'];
 
-// Valideer de gegevens (optioneel)
+
 if (empty($naam) || empty($email) || empty($recensie)) {
     echo "Alle velden zijn verplicht!";
     exit;
 }
 
-// Stuur de gegevens via e-mail
-$to = "jouw_email@voorbeeld.com"; // Vervang met je eigen e-mailadres
+
+$to = "owen.schepers@gmail.com";
 $subject = "Nieuwe recensie van $naam";
 $message = "Naam: $naam\nEmail: $email\nRecensie:\n$recensie";
 $headers = "From: $email";
